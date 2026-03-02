abead79bfc967a187b4ad90f7bebc6cc7652ea10
AjayBhargav Reddy <ajaybhargavreddy44icloud.com@AjayBhargavs-MacBook-Pro-1388.local>
2026-03-02 17:50:28 +0530

    UC2: Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,17 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
+
         System.out.println("=================================");
         System.out.println("   Welcome to Palindrome App   ");
         System.out.println("=================================");
+
+        String word = "madam";
+        String reversed = new StringBuilder(word).reverse().toString();
+
+        if(word.equals(reversed)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
     }
 }
