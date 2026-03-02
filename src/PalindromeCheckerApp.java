b417ff22ab87bff4d08f1b49a18c5232ff50a6e6
AjayBhargav Reddy <ajaybhargavreddy44icloud.com@AjayBhargavs-MacBook-Pro-1388.local>
2026-03-02 17:48:16 +0530

    UC1: Add welcome message module

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,7 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
-        System.out.println("Welcome to Palindrome Checker App");
+        System.out.println("=================================");
+        System.out.println("   Welcome to Palindrome App   ");
+        System.out.println("=================================");
     }
 }
